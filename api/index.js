b39e10e6b404996231aa2e906d7644a65ce9b93f
Ahmadkhaan08 <mrahmadismail96@gmail.com>
2025-11-28 16:43:47 +0500

    updated

--- api/index.js
+++ api/index.js
@@ -53,6 +53,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on PORT 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on PORT 3000");
+// });
+
+export default app
